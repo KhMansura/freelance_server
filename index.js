f6e1a8f81dfc47b8275d43dcf933f8b036186973
KhMansura <iam.khmansura@yandex.com>
2025-11-13 16:23:43 +0600

    added sdk

--- index.js
+++ index.js
@@ -1,127 +1,10 @@
-// const express = require('express');
-// const cors = require('cors');
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// //middleware
-// app.use(cors());
-// app.use(express.json())
-
-// const uri = "mongodb+srv://freelance_market_place:ZttvQebeqBV4Hgrq@cluster0.shyhiog.mongodb.net/?appName=Cluster0";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// // app.get('/hello',(req,res)=>{
-// //   res.send('how are you!')
-
-// // })
-
-
-// async function run() {
-//   try {
-//     await client.connect();
-
-//     const db = client.db('freelance_db')
-//     const jobsCollection = db.collection('jobs');
-
-//     app.get('/jobs' ,async(req,res)=>{
-
-//       console.log(req.query)
-//       const email = req.query.email;
-//       const query ={}
-//       if(email){
-//         query.userEmail = email;
-//       }
-
-
-
-//       const cursor = jobsCollection.find(query);
-//       const result = await cursor.toArray();
-//       res.send(result)
-//     });
-
-//     app.get('/jobs/:id', async(req, res) =>{
-//       const id = req.params.id;
-//       const query = {_id: new ObjectId(id)}
-//       const result = await jobsCollection.findOne(query);
-//       res.send(result);
-
-//     })
-
-//     app.post('/jobs', async(req,res) => {
-//       const newJob = req.body
-//       const result = await jobsCollection.insertOne(newJob);
-//       res.send(result);
-//     })
-//     app.patch('/jobs/:id', async(req,res)=>{
-//       const id = req.params.id;
-//       const updatedJob = req.body;
-//       const query ={_id: new ObjectId(id) }
-//       const update ={
-//         $set: {
-//           name:updatedJob.name,
-//           price:updatedJob.price
-//         }
-//       }
-//       const result = await jobsCollection.updateOne(query,update)
-//       res.send(result)
-
-// })
-//     app.delete('/jobs/:id', async(req,res)=>{
-//       const id = req.params.id;
-//       const query = {_id: new ObjectId(id)};
-//       const result = await jobsCollection.deleteOne(query);
-//       res.send(result);
-//     })
-
-
-
-
-
-
-//     // Send a ping to confirm a successful connection
-// //     await client.db("admin").command({ ping: 1 });
-// //     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-// //   } finally {
-// //     // Ensures that the client will close when you finish/error
-// //     await client.close();
-// //   }
-// // }
-// // run().catch(console.dir);
-
-//  // Confirm connection
-//     await db.command({ ping: 1 });
-//     console.log("✅ Successfully connected to MongoDB!");
-
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-
-// run();
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`server is listening on port ${port}`)
-// })
-
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const admin = require("firebase-admin");
 
+const serviceAccount = require("./serviceKey.json");
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -129,6 +12,13 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 // MongoDB setup
 const uri = process.env.MONGODB_URI;
@@ -156,14 +46,10 @@ async function run() {
     });
 
     // ✅ Get latest 6 jobs
-    // app.get('/jobs/latest', async (req, res) => {
-    //   const result = await jobsCollection.find().sort({ _id: -1 }).limit(6).toArray();
-    //   res.send(result);
-    // });
     app.get('/jobs/latest', async (req, res) => {
   try {
     const result = await jobsCollection.find().sort({ _id: -1 }).limit(6).toArray();
-    res.send(result); // ✅ Always return an array
+    res.send(result);
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
